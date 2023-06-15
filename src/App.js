cee32b7ab280ff8fabecc6d8f7a2fc5b02636ae5
Steve Bersey <berseycfc@aol.com>
2023-06-15 15:59:33 +0100

    added Steve

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
   return (
     <div className='header field'>
-      STEVE's VAT CALCULATOR
+      VAT CALCULATORS
       <div className='colour-border'>
         <VatRateField customstyle="field" vatRateChanged={handleVatRateChanged} value={vatRate} updatePrices={updatePrices} />
         <PriceEntryField customstyle="field" label="Price excl VAT: " priceChanged={handleNetPriceChange} price={netPrice === 0.0 ? "" : netPrice} />
